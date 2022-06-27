e78afa2e059f9ce18e7165cbfb68fdf010d64bb9
Guayabita <92877638+Guayabitaa@users.noreply.github.com>
2022-06-27 13:50:03 -0500

    Create Program2.java

---
+++ Program2.java
@@ -0,0 +1,24 @@
+package dev.panda.java;
+
+public class Program2 {
+    public static void main(String[] args) {
+
+        /*
+        > Greater Than
+        < Smaller Than
+        >= Greater Than // Equal To
+        <= Smaller Than // Equal To
+        == Equal To
+        != Equal To
+        && And
+        || Or
+        */
+
+        if (15 == 5 || 20 == 20) {
+            System.out.println("Code Goes Here!");
+        }
+        else {
+            System.out.println("Code Goes Here!");
+        }
+    }
+}
